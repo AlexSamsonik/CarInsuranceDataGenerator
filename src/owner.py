82382bf1a05d3f5c41d37a54dade3912d059f825
Aliaksandr Samsonik <aliaksandr.samsonik@gmail.com>
2025-03-14 11:23:20 +0100

    'generate_birthdate()' has been added.

--- src/owner.py
+++ src/owner.py
@@ -17,3 +17,12 @@ def generate_last_name() -> str:
     :return: A string representing a last name.
     """
     return Faker().last_name()
+
+
+def generate_birthdate(minimum_age: int = 18, maximum_age: int = 63) -> str:
+    """Generate a birthdate for a person aged between 18 (default) and 63 (default) years.
+
+    :return: A string representing the birth ate in the format 'mm/dd/yyyy'.
+    """
+    birth_date = Faker().date_of_birth(minimum_age=minimum_age, maximum_age=maximum_age)
+    return birth_date.strftime("%m/%d/%Y")
