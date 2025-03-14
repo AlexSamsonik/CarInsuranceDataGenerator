3676cbcefdacdd94f39ee84b685cd56a6194cfe6
Aliaksandr Samsonik <aliaksandr.samsonik@gmail.com>
2025-03-14 11:43:22 +0100

    test has been added to 'generate_birthdate()'.

--- tests/test_owner.py
+++ tests/test_owner.py
@@ -1,8 +1,10 @@
 """Module provides tests for owner part in fake car insurance data."""
 
-from pytest import fixture
+from datetime import datetime
 
-from src.owner import generate_first_name, generate_last_name
+from pytest import fail, fixture
+
+from src.owner import generate_birthdate, generate_first_name, generate_last_name
 
 
 @fixture
@@ -21,6 +23,15 @@ def last_name_fx() -> str:
     :return: A string representing a last name.
     """
     return generate_last_name()
+
+
+@fixture
+def birthdate_fx() -> str:
+    """Fixture to generate a birth ate.
+
+    :return: A string representing the generated birthdate in the format 'mm/dd/yyyy'.
+    """
+    return generate_birthdate()
 
 
 def test_generate_first_name_is_string(first_name_fx):
@@ -53,3 +64,14 @@ def test_generate_last_name_not_empty(last_name_fx):
     :param last_name_fx: The generated last name from the fixture.
     """
     assert len(last_name_fx) > 0, "Last name should not be an empty string"
+
+
+def test_generate_birthdate_format(birthdate_fx):
+    """Test that the generate_birthdate function returns a date in the format 'mm/dd/yyyy'.
+
+    :param birthdate_fx: The generated birthdate from the fixture.
+    """
+    try:
+        datetime.strptime(birthdate_fx, "%m/%d/%Y")
+    except ValueError:
+        fail(f"Birth date '{birthdate_fx}' does not match the format 'mm/dd/yyyy'.")
