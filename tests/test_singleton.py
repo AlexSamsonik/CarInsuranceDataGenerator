5e5bc86015408e328eae7144847d6ce84ff6d7ad
Aliaksandr Samsonik <aliaksandr.samsonik@gmail.com>
2025-04-02 15:17:49 +0200

    Test has been added for FakerSingleton.

--- tests/test_singleton.py
+++ tests/test_singleton.py
@@ -5,6 +5,6 @@ from src.singleton import FakerSingleton
 
 def test_faker_singleton_instance():
     """Test that FakerSingleton always returns the same instance of Faker."""
-    instance1 = FakerSingleton.get_instance()
-    instance2 = FakerSingleton.get_instance()
-    assert instance1 is instance2, "FakerSingleton did not return the same instance."
+    instance_one = FakerSingleton.get_instance()
+    instance_two = FakerSingleton.get_instance()
+    assert instance_one is instance_two, "FakerSingleton did not return the same instance."
